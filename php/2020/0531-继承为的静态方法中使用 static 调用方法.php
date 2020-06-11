486cddaf28aa7afba10ad4af960cbcdb872132e4
imajinyun <imajinyun@gmail.com>
2020-06-11 10:09:09 +0800

    Add some php sample codes

---
+++ php/2020/0531-继承为的静态方法中使用 static 调用方法.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * 在实例化对象时：
+ *
+ * - static：会根据运行时调用的类来决定实例化对象；
+ * - self：则是根据所在位置的类来决定实例化对象；
+ *
+ * 注意：只想实例化子类，并且不希望后续在对子类的使用中由于父类的变化对子类产生影响时，后期静态绑定就能发挥它的作用了。
+ */
+
+class A
+{
+    public static function who()
+    {
+        echo __CLASS__, PHP_EOL;
+    }
+
+    public static function foo()
+    {
+        static::who();
+    }
+}
+
+class B extends A
+{
+    public static function who()
+    {
+        echo __CLASS__, PHP_EOL;
+    }
+
+    public static function foo()
+    {
+        echo __CLASS__, PHP_EOL;
+    }
+
+    public static function test()
+    {
+        A::foo();
+        parent::foo();
+        static::foo();
+        self::foo();
+    }
+}
+
+class C extends B
+{
+    public static function who()
+    {
+        echo __CLASS__, PHP_EOL;
+    }
+
+    public static function foo()
+    {
+        echo __CLASS__, PHP_EOL;
+    }
+}
+
+/*
+A
+C
+C
+B
+*/
+C::test();
