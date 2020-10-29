261400ebb7b2fbfd1e2c3adf4a14046476e9d1b3
imajinyun <imajinyun@gmail.com>
2020-10-29 14:19:38 +0800

    Add src/a/a0011/main.go

---
+++ go/src/a/a0011/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int
+	i = 1
+	fmt.Println("Print prime number:")
+
+Looper:
+	for i < 100 {
+		i++
+		for j = 2; j < i; j++ {
+			if i%j == 0 {
+				goto Looper
+			}
+		}
+		fmt.Printf("%d ", i)
+	}
+	fmt.Println()
+}
