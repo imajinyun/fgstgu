ba82477629191e897704dce05f52b912c4a885ec
imajinyun <imajinyun@gmail.com>
2020-10-30 08:15:51 +0800

    Add src/a/a0013/main.go

---
+++ go/src/a/a0013/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// 函数调用
+func main() {
+	a := strToCase("ABCDEFGHIJKLMNOPQRSTUVWXYZ", toLower)
+	fmt.Printf("String to lower: %s\n", a)
+
+	b := strToCase2("abcdefghijklmnopqrstuvwxyz", toUpper)
+	fmt.Printf("String to upper: %s\n", b)
+}
+
+func toLower(str string) string {
+	result := ""
+	for _, value := range str {
+		result += strings.ToLower(string(value))
+	}
+	return result
+}
+
+func toUpper(str string) string {
+	result := ""
+	for _, value := range str {
+		result += strings.ToUpper(string(value))
+	}
+	return result
+}
+
+func strToCase(str string, fn func(string) string) string {
+	fmt.Printf("fn type: %T\n", fn)
+	return fn(str)
+}
+
+type caseFunc func(string) string
+
+func strToCase2(str string, fn caseFunc) string {
+	fmt.Printf("fn type: %T\n", fn)
+	return fn(str)
+}
