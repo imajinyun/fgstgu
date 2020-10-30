b7b2e5026bd15da9ed8fdd99bdd58c51306a49d3
imajinyun <imajinyun@gmail.com>
2020-10-30 10:47:27 +0800

    Add src/a/a0018/main.go

---
+++ go/src/a/a0018/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum, avg, cnt := score(90, 85.5, 70, 66, 77, 65.5)
+	fmt.Printf("cnt = %d, sum = %.2f, avg = %.2f\n", cnt, sum, avg)
+
+	scores := []float64{92, 88, 71.5, 69, 70.5, 88}
+	sum, avg, cnt = score(scores...)
+	fmt.Printf("cnt = %d, sum = %.2f, avg = %.2f\n", cnt, sum, avg)
+}
+
+func score(scores ...float64) (sum, avg float64, cnt int) {
+	for _, value := range scores {
+		sum += value
+		cnt++
+	}
+	avg = sum / float64(cnt)
+	return
+}
