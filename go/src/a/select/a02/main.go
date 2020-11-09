6750fc0b3c720e588f010024e41b0ab83fc30ef7
imajinyun <imajinyun@gmail.com>
2020-11-09 14:08:21 +0800

    Add src/a/select/a02/main.go

---
+++ go/src/a/select/a02/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	a := make(chan int)
+	b := make(chan int)
+	go func() {
+		data := <-a
+		fmt.Println("a channel data: ", data)
+	}()
+	go func() {
+		data := <-b
+		fmt.Println("b channel data: ", data)
+	}()
+
+	select {
+	case a <- 100:
+		close(a)
+		fmt.Println("Write data to a channel")
+	case b <- 200:
+		close(b)
+		fmt.Println("Write data to b channel")
+	case <-time.After(2 * time.Millisecond):
+		fmt.Println("Executing program in delay channel")
+	}
+	time.Sleep(4 * time.Second)
+	fmt.Println("The main func is over!")
+}
