4e168e73de8fbbb10f37d08c21ec9b86be3afc56
imajinyun <imajinyun@gmail.com>
2020-11-11 08:19:05 +0800

    Add src/a/flow-control/a05/main.go

--- go/src/a/flow-control/a05/main.go
+++ go/src/a/flow-control/a05/main.go
@@ -3,13 +3,26 @@ package main
 import "fmt"
 
 func main() {
-Looper:
+Looper1:
 	for i := 0; i < 2; i++ {
 		for j := 0; j < 5; j++ {
 			switch j {
 			case 2:
 				fmt.Println(i, j)
-				continue Looper
+				continue Looper1
+			}
+		}
+	}
+
+	fmt.Println()
+
+Looper2:
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 5; j++ {
+			switch j {
+			case 2:
+				fmt.Println(i, j)
+				break Looper2
 			}
 		}
 	}
