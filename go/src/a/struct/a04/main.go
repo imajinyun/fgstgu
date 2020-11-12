2f6727cb2d87fdc15a05b511c2179c523c5e446e
imajinyun <imajinyun@gmail.com>
2020-11-12 10:10:32 +0800

    Update src/a/struct/a04/main.go

--- go/src/a/struct/a04/main.go
+++ go/src/a/struct/a04/main.go
@@ -19,11 +19,34 @@ type Car struct {
 	Engine
 }
 
+// RefitCar struct.
+type RefitCar struct {
+	Wheel
+	Engine struct {
+		Power int
+		Type  string
+	}
+}
+
 func main() {
-	c := Car{
+	c1 := Car{
 		Wheel:  Wheel{Size: 23},
 		Engine: Engine{Power: 115, Type: "1.8T"},
 	}
-	fmt.Printf("%+v\n", c)
-	fmt.Printf("%v\n", c)
+	fmt.Printf("%+v\n", c1)
+	fmt.Printf("%v\n", c1)
+	fmt.Println()
+
+	c2 := RefitCar{
+		Wheel: Wheel{Size: 20},
+		Engine: struct {
+			Power int
+			Type  string
+		}{
+			Power: 150,
+			Type:  "2.0T",
+		},
+	}
+	fmt.Printf("%+v\n", c2)
+	fmt.Printf("%v\n", c2)
 }
