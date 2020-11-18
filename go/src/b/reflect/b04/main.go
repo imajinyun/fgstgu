a0267f699eb1292a634bdc65757b78d34de44f38
imajinyun <imajinyun@gmail.com>
2020-11-18 10:20:49 +0800

    Add src/b/reflect/b04/main.go

---
+++ go/src/b/reflect/b04/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	type cat struct {
+		Name string
+		Type int `type:"info" id:"100" message:"Hello World!"`
+	}
+	ins := cat{"Hello", 100}
+	typeOfCat := reflect.TypeOf(ins)
+	for i := 0; i < typeOfCat.NumField(); i++ {
+		fieldType := typeOfCat.Field(i)
+		fmt.Printf("Name: %v, Tag: %v\n", fieldType.Name, fieldType.Tag)
+	}
+
+	if catType, ok := typeOfCat.FieldByName("Type"); ok {
+		fmt.Println(
+			catType.Tag.Get("type"),
+			catType.Tag.Get("id"),
+			catType.Tag.Get("message"))
+	}
+}
