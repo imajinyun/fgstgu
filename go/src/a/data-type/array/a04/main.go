3d3bc1e24618d07dbb784a885ce9d10b25612dbf
imajinyun <imajinyun@gmail.com>
2020-12-02 18:03:37 +0800

    Update src/a/data-type/array/a04/main.go

--- go/src/a/data-type/array/a04/main.go
+++ go/src/a/data-type/array/a04/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var a = [3]int{}
+	var a = [3]int{0}
 	b := [3]int{}
 	c := [...]int{}
+	d := [...]int{}
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
 	fmt.Println()
+
+	if a == b {
+		fmt.Println("a == b is", a == b)
+	}
+	if c == d {
+		fmt.Println("c == d is", c == d)
+	}
 
 	// invalid operation: c == nil (mismatched types [0]int and nil)
 	// if c == nil {
