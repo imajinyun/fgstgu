7703c529bdc2e09665f83278c9bbc547d80cc45c
imajinyun <imajinyun@gmail.com>
2020-12-14 19:34:23 +0800

    Add c/fsnotify/c01/main.go

---
+++ go/src/c/fsnotify/c01/go.mod
@@ -0,0 +1,5 @@
+module fsnotify/c01
+
+go 1.15
+
+require github.com/fsnotify/fsnotify v1.4.9 // indirect
---
+++ go/src/c/fsnotify/c01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+// -> go run main.go
+// echo "some content..." -> /tmp/foo
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				log.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					log.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("/tmp/foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
