54af5df738632f62897777a9c4dd2de1fa0ec18d
imajinyun <imajinyun@gmail.com>
2020-12-15 16:41:50 +0800

    Add src/b/math/b02/main.go

---
+++ go/src/b/math/b02/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func main() {
+	numbers := generates(1e7)
+	fmt.Println(add(numbers))
+	fmt.Println(addConcurrent(runtime.NumCPU(), numbers))
+}
+
+func generates(n int) []int{
+	numbers := make([]int, n)
+	for i := 0; i < n; i++ {
+		numbers[i] = rand.Intn(n)
+	}
+	return numbers
+}
+
+func add(numbers []int) int {
+	var v int
+	for _, n := range numbers {
+		v += n
+	}
+	return v
+}
+
+func addConcurrent(goroutines int, numbers []int) int {
+	var v int64
+	totalNumbers := len(numbers)
+	lastGoroutine := goroutines - 1
+	stride := totalNumbers / goroutines
+
+	var wg sync.WaitGroup
+	wg.Add(goroutines)
+
+	for g := 0; g < goroutines; g++ {
+		go func(g int) {
+			start := g * stride
+			end := start + stride
+			if g == lastGoroutine {
+				end = totalNumbers
+			}
+			var t int
+			for _, n := range numbers[start:end] {
+				t += n
+			}
+			atomic.AddInt64(&v, int64(t))
+			wg.Done()
+		}(g)
+	}
+	wg.Wait()
+	return int(v)
+}
