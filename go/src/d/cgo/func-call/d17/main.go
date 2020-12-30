9aa7d6b2f69388b717ed9a8e5d6309062db8f5fd
imajinyun <imajinyun@gmail.com>
2020-12-30 08:58:07 +0800

    Update src/d/cgo/func-call/d17/main.go

--- go/src/d/cgo/func-call/d17/main.go
+++ go/src/d/cgo/func-call/d17/main.go
@@ -14,6 +14,7 @@ func goQsortCompare(a, b unsafe.Pointer) C.int {
 	return C.int(*pa - *pb)
 }
 
+// -> go run main.go
 func main() {
 	values := []int32{18, 101, 0, 1, 89, 23, 78, 2, 23, 9}
 	qsort.Sort(unsafe.Pointer(&values[0]),
