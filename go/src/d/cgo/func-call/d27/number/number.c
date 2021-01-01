ab44251b754a417925ac54102623765fad9e0d9e
imajinyun <imajinyun@gmail.com>
2021-01-01 19:28:25 +0800

    Add src/d/cgo/func-call/d{27,28}

---
+++ go/src/d/cgo/func-call/d27/number/number.c
@@ -0,0 +1,3 @@
+#include "number.h"
+
+int number_add_mod(int a, int b, int mod) { return (a + b) % mod; }
---
+++ go/src/d/cgo/func-call/d28/number/number.c
@@ -0,0 +1,6 @@
+#include "number.h"
+
+int average(int a, int b) {
+  int avg = (a + b) / 2;
+  return avg;
+}
