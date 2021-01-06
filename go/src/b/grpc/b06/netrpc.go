eb7db0208e14730f55989922323d1a5d98f779a9
imajinyun <imajinyun@gmail.com>
2021-01-06 10:58:10 +0800

    Add src/b/grpc/b07/netrpc.go

--- go/src/b/grpc/b06/netrpc.go
+++ go/src/b/grpc/b06/netrpc.go
@@ -9,6 +9,7 @@ type netrpcPlugin struct{ *generator.Generator }
 
 func (p *netrpcPlugin) Name() string                { return "netrpc" }
 func (p *netrpcPlugin) Init(g *generator.Generator) { p.Generator = g }
+
 func (p *netrpcPlugin) GenerateImports(file *generator.FileDescriptor) {
 	if len(file.Service) > 0 {
 		p.genImportCode(file)
@@ -19,9 +20,11 @@ func (p *netrpcPlugin) Generate(file *generator.FileDescriptor) {
 		p.genServiceCode(svc)
 	}
 }
+
 func (p *netrpcPlugin) genImportCode(file *generator.FileDescriptor) {
 	p.P("// TODO: import code")
 }
+
 func (p *netrpcPlugin) genServiceCode(svc *descriptor.ServiceDescriptorProto) {
 	p.P("// TODO: service code, Name = " + svc.GetName())
 }
