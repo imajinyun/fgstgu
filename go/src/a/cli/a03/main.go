787ac757012f4cb68dae1ba6a5c3a7600bfbefb6
imajinyun <imajinyun@gmail.com>
2021-01-19 08:14:42 +0800

    Add src/a/cli/a05/main.go

--- go/src/a/cli/a03/main.go
+++ go/src/a/cli/a03/main.go
@@ -1,3 +1,17 @@
+/*
+格式化输出占位符：
+%d          十进制整数
+%x, %o, %b  十六进制，八进制，二进制整数
+%f, %g, %e  浮点数： 3.141593 3.141592653589793 3.141593e+00
+%t          布尔：true 或 false
+%c          字符（rune），Unicode 码点
+%s          字符串
+%q          带双引号的字符串 "abc" 或带单引号的字符 'c'
+%v          变量的自然形式（natural format）
+%T          变量的类型
+%%          字面上的百分号标志（无操作数）
+*/
+
 package main
 
 import (
@@ -8,29 +22,21 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "err: %v\n", err)
-				continue
-			}
-			f.Close()
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		if input.Text() == "EOF" {
+			fmt.Printf("✔️ your input `EOF` quit\n")
+			break
 		}
+		fmt.Fprintf(os.Stdout, "✔️ your input: "+input.Text()+"\n")
+		counts[input.Text()]++
 	}
+
+	// NOTE: ignoring potential errors from input.Err()
+	fmt.Printf("✔️ your input count result:\n")
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
 		}
-	}
-}
-
-func countLines(f *os.File, counts map[string]int) {
-	input := bufio.NewScanner(f)
-	for input.Scan() {
-		counts[input.Text()]++
 	}
 }
---
+++ go/src/a/cli/a05/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// -> go run ./main.go /usr/local/var/log/nginx/error.log
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "err: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
