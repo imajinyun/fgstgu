da7174efd5e4b4ba47f3e1a31d94b82f59a27be2
imajinyun <imajinyun@gmail.com>
2021-01-20 14:33:42 +0800

    Add src/a/regex/0011.匹配系统路径.py

---
+++ python/src/a/regex/0011.匹配系统路径.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import re
+
+
+def solution() -> None:
+    a, b = '/usr/local/bin/python3', 'C:\\Program Files\\Python 3.8.4\\Python.exe'
+    items = [
+        # 匹配 Linux/Unix 路径
+        re.search(r'^.*/', a).group(0),  # /usr/local/bin/
+        re.search(r'[^/]*$', a).group(0),  # python3
+
+        # 匹配 Windows 路径
+        re.search(r'^.*\\', b).group(0),  # C:\Program Files\Python 3.8.4\
+        re.search(r'[^\\]*$', b).group(0),  # Python.exe
+    ]
+    for i in range(len(items)):
+        print(items[i])
+
+
+if __name__ == "__main__":
+    solution()
