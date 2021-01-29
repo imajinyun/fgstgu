2c49bdca746bd3140ed92b71731ea1b8dc5a08fd
imajinyun <imajinyun@gmail.com>
2021-01-29 13:48:34 +0800

    Update src/a/regex/0025.浮点数匹配.py

--- python/src/a/regex/0025.浮点数匹配.py
+++ python/src/a/regex/0025.浮点数匹配.py
@@ -4,8 +4,14 @@ import re
 
 
 def solution() -> None:
-    # 必须出现的匹配
-    regex1 = r'(\+?(\d+|\.\d+|\d+\.\d+)|\-?(\d+|\d+\.\d+))'
+    """
+    必须出现的匹配。
+
+    以下正则表达式并不能过滤 2021.01.31 这样的字符串：
+
+    r'(\+?(\d+|\.\d+|\d+\.\d+)|\-?(\d+|\d+\.\d+))'
+    """
+    regex1 = r'^(?!<\.)(\+?(\d+|\.\d+|\d+\.\d+)|-?(\d+|\d+\.\d+))(?!\.)$'
     items1 = [
         re.search(regex1, '3.1415926') is not None,  # True
         re.search(regex1, '0.1415926') is not None,  # True
@@ -16,6 +22,7 @@ def solution() -> None:
         re.search(regex1, '.3') is not None,  # True
         re.search(regex1, '+.3') is not None,  # True
         re.search(regex1, '-0.3') is not None,  # True
+        re.search(regex1, '2021.01.28') is not None,  # False
     ]
     regex2 = r'^(?=.+)(?:[1-9]\d*|0)?(?:\.\d+)?$'
     items2 = [
@@ -24,12 +31,13 @@ def solution() -> None:
         re.search(regex2, '1000.00001415926') is not None,  # True
         re.search(regex2, '0.000001') is not None,  # True
         re.search(regex2, '.000001') is not None,  # True
-        re.search(regex2, '+0.1415926') is not None,  # False
-        re.search(regex2, '-1000.1415926') is not None,  # False
         re.search(regex1, '10') is not None,  # True
         re.search(regex2, '.3') is not None,  # True
+        re.search(regex2, '+0.1415926') is not None,  # False
+        re.search(regex2, '-1000.1415926') is not None,  # False
         re.search(regex2, '+.3') is not None,  # False
         re.search(regex2, '-0.3') is not None,  # False
+        re.search(regex1, '2021.01.28') is not None,  # False
     ]
     [print(items1[i]) for i in range(len(items1))]
     print('\n')
