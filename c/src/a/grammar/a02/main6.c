e38974efbc9f3176316fc328021fe71fabb7154f
imajinyun <imajinyun@gmail.com>
2021-01-31 11:56:48 +0800

    Update src/a/grammar/a02/main6.c

--- c/src/a/grammar/a02/main6.c
+++ c/src/a/grammar/a02/main6.c
@@ -9,9 +9,14 @@ int main(int argc, char *argv[]) {
   scanf("%d", &number);
   val = number;
   while (val) {
-    sum = sum + val % 10;
+    sum = sum * 10 + val % 10;
     val /= 10;
     printf("sum = %d, val = %d\n", sum, val);
+  }
+  if (sum == number) {
+    printf("输入的 %d 是回文数\n", number);
+  } else {
+    printf("输入的 %d 不是回文数\n", number);
   }
   return 0;
 }
