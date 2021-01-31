8c8f7a7dac34706dba06b6502c1943c81ab01587
imajinyun <imajinyun@gmail.com>
2021-01-31 11:11:08 +0800

    Add src/a/grammar/a02/main5.c

---
+++ c/src/a/grammar/a02/main5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  puts("换行符：[\n]");
+  puts("制表符：[\t]");
+  puts("单引号：[\']");
+  puts("双引号：[\"]");
+  puts("反斜杠：[\\]");
+
+  puts("C\nC++\tJava\n\"\'\"\\");
+  return 0;
+}
