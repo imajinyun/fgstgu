6ce65d21a7850a7d92b5e1db0ad40dd0c847c502
imajinyun <imajinyun@gmail.com>
2021-02-18 17:19:15 +0800

    Add src/a/enum/0001.枚举类型.c

---
+++ c/src/a/enum/0001.枚举类型.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  enum weeks { Mon = 1, Tue, Wed, Thu, Fri, Sat, Sun } day;
+  printf("Please enter a number between 1 and 7: ");
+  scanf("%d", &day);
+  switch (day) {
+    case Mon:
+      puts("Monday");
+      break;
+    case Tue:
+      puts("Tuesday");
+      break;
+    case Wed:
+      puts("Wednesday");
+      break;
+    case Thu:
+      puts("Thursday");
+      break;
+    case Fri:
+      puts("Friday");
+      break;
+    case Sat:
+      puts("Saturday");
+      break;
+    case Sun:
+      puts("Sunday");
+      break;
+    default:
+      puts("Error");
+      exit(EXIT_FAILURE);
+      break;
+  }
+  return 0;
+}
